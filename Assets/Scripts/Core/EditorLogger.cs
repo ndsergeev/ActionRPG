60dc4c9ec4f794c2ada54ada121919f7ec314ba5
ndsergeev <sergeevkep@icloud.com>
2022-01-04 23:48:10 +1100

    Squashed update dev into dev-input:
    commit ae8d0f891d7d20a142ddc242c314144f7f2b75f5
    Author: ndsergeev <sergeevkep@icloud.com>
    Date:   Tue Jan 4 16:46:33 2022 +1100
    
        Added Assert
    
    commit a588590ad97675e61d0726f752eae52cbc166113
    Author: ndsergeev <sergeevkep@icloud.com>
    Date:   Tue Jan 4 00:05:09 2022 +1100
    
        Singleton to Lazy
    
    commit ac0a7e1f02489c796bb5105008b5c830936cafc3
    Author: ndsergeev <sergeevkep@icloud.com>
    Date:   Fri Dec 31 12:48:30 2021 +1100
    
        Added class for hashed properties
    
    commit a8d66cbdcb0668ac74d92628707c2bf5c50f2a0d
    Author: ndsergeev <sergeevkep@icloud.com>
    Date:   Fri Dec 31 12:30:48 2021 +1100
    
        Added EditorLogger to exclude Debug.Log from Build
    
    commit 35b90ce32aa6562c4f3235af3e6555cb332adee7
    Author: ndsergeev <sergeevkep@icloud.com>
    Date:   Thu Dec 30 17:27:00 2021 +1100
    
        Squashed dev-input into dev:
    
        commit d303587088241798672b09cf1f3c70e5a69e3bcf
        Author: ndsergeev <sergeevkep@icloud.com>
        Date:   Thu Dec 30 17:26:11 2021 +1100
    
            Added input events
    
        commit 2d9bc15fa6c826e95513f7d147d9f0e9a3420835
        Author: ndsergeev <sergeevkep@icloud.com>
        Date:   Thu Dec 30 16:16:19 2021 +1100
    
            Added and referenced empty Input
    
    commit 5e8d5191d0507509530878c52171655b10a1ddc6
    Author: ndsergeev <sergeevkep@icloud.com>
    Date:   Thu Dec 30 16:16:40 2021 +1100
    
        Squashed dev-input into dev:
    
        commit 2d9bc15fa6c826e95513f7d147d9f0e9a3420835
        Author: ndsergeev <sergeevkep@icloud.com>
        Date:   Thu Dec 30 16:16:19 2021 +1100
    
            Added and referenced empty Input

---
+++ Assets/Scripts/Core/EditorLogger.cs
@@ -0,0 +1,50 @@
+
+namespace Main.Core.Console
+{
+    using System.Diagnostics;
+    using Debug = UnityEngine.Debug;
+    
+    /// <summary>
+    /// Is a wrapper of UnityEngine.Debug.[Log, LogWarning, LogError] to exclude from builds.
+    /// Even thought it is a wrapper and should be excluded from the build, still better to search
+    /// through this class for its usage in the project and remove once is a release version.
+    /// </summary>
+    public static class EditorLogger
+    {
+        private const string ConditionalString = "UNITY_EDITOR";
+
+        [Conditional(ConditionalString)]
+        public static void Log(object obj)
+            => Debug.Log(obj);
+        
+        [Conditional(ConditionalString)]
+        public static void Log(string format, params object[] args)
+            => Debug.LogFormat(format, args);
+        
+        [Conditional(ConditionalString)]
+        public static void LogWarning(object obj)
+            => Debug.LogWarning(obj);
+        
+        [Conditional(ConditionalString)]
+        public static void LogWarning(string format, params object[] args)
+            => Debug.LogWarningFormat(format, args);
+        
+        [Conditional(ConditionalString)]
+        public static void LogError(object obj)
+            => Debug.LogError(obj);
+        
+        [Conditional(ConditionalString)]
+        public static void LogError(string format, params object[] args)
+            => Debug.LogErrorFormat(format, args);
+        
+        [Conditional(ConditionalString)]
+        public static void Assert(bool condition, string format)
+            => Debug.Assert(condition, format);
+        
+        [Conditional(ConditionalString)]
+        public static void Assert(bool condition, string format, params object[] args)
+            => Debug.AssertFormat(condition, format, args);
+        
+        // add more Debug-related methods if needed
+    }
+}
